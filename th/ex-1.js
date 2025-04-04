cda8449a6808752f1f7258e4c708abdd986bb78a
Phanupong-M <panupong38240@gmail.com>
2025-04-04 12:00:57 +0700

    submitted algorithm

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,9 +1,21 @@
 const moveZeroes = function (nums) {
-  //Start coding here
-};
+   for (i = 0; i < nums.length-1; i++){
+    for (j = 0; j < nums.length-1-i; j++){
+      if (nums[j] === 0){
+         let temp = nums[j+1]
+         nums[j+1] = nums[j]
+         nums[j] = temp
+      }
+    }
+   }
+   return nums
+}
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
 
+
 const result2 = moveZeroes([0]);
 console.log(result2); // [0]
+
+
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,6 +1,22 @@
 const romanToInt = function (s) {
-  //Start coding here
-};
+  romanNumber = {I: 1, V:5, X:10, L: 50, C: 100, D: 500, M: 1000}
+  cur_value = 0
+  prev_value = 0
+  sum = 0
+
+    for (i = s.length-1; i >= 0; i--){
+        cur_value = romanNumber[s[i]]
+
+        if (cur_value < prev_value) {
+          sum -= cur_value
+        } else{
+          sum += cur_value
+        }
+    prev_value = cur_value
+  }
+  
+  return sum
+}
 
 const result1 = romanToInt("III"); // 3
 console.log(result1);
@@ -10,3 +26,5 @@ console.log(result2);
 
 const result3 = romanToInt("MCMXCIV"); //1994
 console.log(result3);
+
+
